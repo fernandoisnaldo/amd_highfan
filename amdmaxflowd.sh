61b5bab0bacd5970c86c8621b32592c4d0b2d6db
Fernando Isnaldo Silva de Faria <33297740+fernandoisnaldo@users.noreply.github.com>
2022-09-07 14:32:58 -0300

    Update amdmaxflowd.sh
    Reduces log

--- amdmaxflowd.sh
+++ amdmaxflowd.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
-min_diff=4000 #difference of temperature in celsius: t*1000, where turn on pwm manual mode
-auto_diff=20000 #difference of in celsius: t*1000, where return pwm mode to auto
-max_pwm=170 #max value: 255; can be set according to your noise tolerance.
-#get pwm state
-pwm_mode=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable)
-#get sensors
-edge=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp1_input)
-edge_limit=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp1_crit)
-junction=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp2_input)
-junction_limit=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp2_crit)
-mem=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp3_input)
-mem_limit=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp3_crit)
-#compute differences
-edge_diff=$[$edge_limit-$edge]
-junction_diff=$[$junction_limit-$junction]
-mem_diff=$[$mem_limit-$mem]
-#high temp test
-if [ $edge_diff -le $min_diff ] || [ $junction_diff -le $min_diff ] || [ $mem_diff -le $min_diff ]; then
-	echo "1" > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable
-	echo "$max_pwm" > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1
-#normal temp test
-elif [ $edge_diff -ge $auto_diff ] && [ $junction_diff -ge $auto_diff ] && [ $mem_diff -ge $auto_diff ] && [ $pwm_mode -ne 2 ]; then
-	echo "2" > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable
-fi
+min_diff=4000
+auto_diff=20000 
+max_pwm=200 #max value: 255
+while true
+do
+	sleep 1
+	pwm_mode=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable)
+	#get sensors
+	edge=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp1_input)
+	edge_limit=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp1_crit)
+	junction=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp2_input)
+	junction_limit=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp2_crit)
+	mem=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp3_input)
+	mem_limit=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp3_crit)
+	#compute differences
+	edge_diff=$[$edge_limit-$edge]
+	junction_diff=$[$junction_limit-$junction]
+	mem_diff=$[$mem_limit-$mem]
+	#high temp test
+	if [ $edge_diff -le $min_diff ] || [ $junction_diff -le $min_diff ] || [ $mem_diff -le $min_diff ]; then
+		echo "1" > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable
+		echo "$max_pwm" > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1
+	#normal temp test
+	elif [ $edge_diff -ge $auto_diff ] && [ $junction_diff -ge $auto_diff ] && [ $mem_diff -ge $auto_diff ] && [ $pwm_mode -ne 2 ]; then
+		echo "2" > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable
+	fi
+done
